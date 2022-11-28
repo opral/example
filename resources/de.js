3839d93b9cfff418d949a0c384fa2570fef2566a
samuelstroschein <>
2022-11-28 12:38:05 +0100

    inlang: update translations

--- resources/de.js
+++ resources/de.js
@@ -1,6 +1,6 @@
 export default {
   "hello": "hallo {{name}}, wie geht es dir?",
-  "shoeDescription": "Dies ist toll",
+  "shoeDescription": "Dies ist ein brillianter Schuh!",
   "new": "NEU",
   "inputQuestion": "Wie lautet der Name des Schuhs?"
 }
