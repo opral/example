75ca57c2219e2e63d1105d47995828dcc4698898
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-11-28 13:58:59 +0100

    clean translations

--- resources/de.js
+++ resources/de.js
@@ -1,9 +1,3 @@
 export default {
   "hello": "hallo {{name}}, wie geht es dir?",
-  "shoeDescription": "Dies ist ein brillianter Schuh",
-  "new": "NEU",
-  "inputQuestion": "Wie lautet der Name des Schuhs",
-  "categoryFashion": "Mode",
-  "categoryProducts": "Produkte",
-  "shoeCardTitle": "Kaufe {{name}}"
 }
