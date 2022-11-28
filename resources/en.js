b2683ecedca8bc49ff21d070793a547a6b17d1da
samuelstroschein <>
2022-11-28 12:20:45 +0100

    inlang: update translations

--- resources/en.js
+++ resources/en.js
@@ -1,9 +1,8 @@
 export default {
-  shoeCardTitle: "Buy {{name}}",
-  inputQuestion: "What is the name of the shoe?",
-  new: "NEW",
-  shoeDescription:
-    "This is a brilliant shoe that you should buy! Or maybe the image is displaying something else?",
-  categoryFashion: "Fashion",
-  categoryProducts: "Products",
-};
+  "shoeCardTitle": "Buy {{name}}",
+  "inputQuestion": "What is the name of the shoe?",
+  "new": "NEW",
+  "shoeDescription": "This is a brilliant shoe that you should buy! Or maybe the image is displaying something else?",
+  "categoryFashion": "Fashion",
+  "categoryProducts": "Products"
+}
