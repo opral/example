c151465b38e99212543bb8f4b97769c706ce016f
samuelstroschein <>
2022-11-28 13:19:19 +0100

    inlang: update translations

--- resources/de.js
+++ resources/de.js
@@ -1,6 +1,6 @@
 export default {
   "hello": "hallo {{name}}, wie geht es dir?",
-  "shoeDescription": "Dies ist ein brillianter Schuh!",
+  "shoeDescription": "Dies ist ein brillianter Schuh!d",
   "new": "NE",
   "inputQuestion": "Wie lautet der Name des Schuhs?",
   "categoryFashion": "Mode",
