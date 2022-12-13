f4ff4f1b246307281e055e627a25f2876dc044a2
samuelstroschein <>
2022-12-13 15:04:36 +0100

    inlang: update translations

--- resources/de.js
+++ resources/de.js
@@ -1,6 +1,6 @@
 export default {
   "hello": "hallo {{name}}, wie geht es dir?",
   "shoeCardTitle": "Kaufe {{name}}",
-  "new": "Neu",
+  "new": "Ne",
   "inputQuestion": "Wie lautet der Name des"
 }
