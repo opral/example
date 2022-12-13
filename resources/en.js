ac1d3acca93e3bdd5a9e1f0893696de58c4e8ab1
jannesblobel <>
2022-12-13 22:10:58 +0100

    inlang: update translations

--- resources/en.js
+++ resources/en.js
@@ -1,6 +1,6 @@
 export default {
   "shoeCardTitle": "Buy {{name}}",
-  "inputQuestion": "What is the name of the shoe?",
+  "inputQuestion": "What is the name of the shoe? ",
   "new": "NEW",
   "shoeDescription": "This is a brilliant shoe that you should buy! Or maybe the image is displaying something else?",
   "categoryFashion": "Fashion",
