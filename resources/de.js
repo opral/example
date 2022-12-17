be1a433cc24f35f243ef052c5d1929d9886828fe
jannesblobel <>
2022-12-17 21:43:59 +0100

    inlang: update translations

--- resources/de.js
+++ resources/de.js
@@ -2,7 +2,7 @@ export default {
   "hello": "hallo {{name}}, wie geht es dir?",
   "shoeCardTitle": "Kaufe {{name}}",
   "new": "Neu!!!!",
-  "inputQuestion": "Wie lautet der Name von den Schuhen",
+  "inputQuestion": "",
   "categoryFashion": "Mode",
   "shoeDescription": "i guess ja mal gucken ob das geht "
 }
