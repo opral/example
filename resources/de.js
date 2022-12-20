0fb6923195b5cbd84a36ea1a9be0a74e6cb1fada
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-12-20 17:00:00 +0100

    update

--- resources/de.js
+++ resources/de.js
@@ -1,8 +1,8 @@
 export default {
-  "hello": "hallo {{name}}, wie geht es dir?",
-  "shoeCardTitle": "Kaufe {{name}}",
-  "new": "Neu",
-  "inputQuestion": "Wie ist der Name des Schuhs?",
-  "categoryFashion": "Mode",
-  "shoeDescription": "i guess ja mal gucken ob das geht "
-}
+  hello: "hallo {{name}}, wie geht es dir?",
+  shoeCardTitle: "Kaufe {{name}}",
+  new: "Neu",
+  inputQuestion: "Wie ist der Name des Schuhs?",
+  categoryFashion: "Mode",
+  shoeDescription: "Ich gucke mal ja mal gucken ob das geht ",
+};
