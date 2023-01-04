c944d527f71bf83d5b4b238ec6ecf46a8f24eb3d
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-01-04 12:57:06 +0100

    udpate commit change

--- inlang.config.js
+++ inlang.config.js
@@ -1,22 +1,22 @@
-/**
- * @type {import("@inlang/core/config").InitializeConfig}
- */
 export async function initializeConfig(env) {
-  // importing https://github.com/samuelstroschein/inlang-plugin-json
   const plugin = await env.$import(
     "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1.0.0/dist/index.js"
   );
 
   const pluginConfig = {
-    pathPattern: "./resources/{language}.json",
+    pathPattern: "./{language}.json",
   };
 
   return {
     referenceLanguage: "en",
     languages: ["en", "de", "fr"],
-    readResources: (args) =>
-      plugin.readResources({ ...args, ...env, pluginConfig }),
-    writeResources: (args) =>
-      plugin.writeResources({ ...args, ...env, pluginConfig }),
+    readResources: (args) => {
+      return plugin.readResources({ ...args, ...env, pluginConfig });
+      // reading resources
+    },
+    writeResources: (args) => {
+      // writing resources
+      return plugin.writeResources({ ...args, ...env, pluginConfig });
+    },
   };
 }
