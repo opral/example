9dc1f634eff34e2693724fcc0a3a889c17311272
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-01-20 21:37:38 -0500

    use latest plugin version

--- inlang.config.js
+++ inlang.config.js
@@ -5,13 +5,13 @@
  */
 export async function defineConfig(env) {
   const plugin = await env.$import(
-    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1.0.0/dist/index.js"
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
   );
 
   const pluginConfig = {
     pathPattern: "./resources/{language}.json",
   };
-  
+
   return {
     referenceLanguage: "en",
     languages: ["en", "de", "fr"],
