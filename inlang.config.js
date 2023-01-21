4ecfa9f8755e0e15c7dd85ec12412630e3100bf2
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-01-20 21:31:04 -0500

    chore: @inlang/core 0.3.0

--- inlang.config.js
+++ inlang.config.js
@@ -1,9 +1,9 @@
 /**
  * Using JSDoc to get typesafety.
  *
- * @type {import("@inlang/core/config").InitializeConfig}
+ * @type {import("@inlang/core/config").DefineConfig}
  */
-export async function initializeConfig(env) {
+export async function defineConfig(env) {
   const plugin = await env.$import(
     "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1.0.0/dist/index.js"
   );
