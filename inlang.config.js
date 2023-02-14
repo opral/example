17cd4968266b1c76be699a892cd2edfea8508491
Robin Bühler <1105080+openscript@users.noreply.github.com>
2023-02-14 21:52:54 +0100

    feat: add ideExtension to config (#10)
    * feat: add ideExtension to config
    
    * fix: undefined process global
    
    * use optional chaining
    
    This should definitely fix the "process is undefined" bug (hopefully)
    
    ---------
    
    Co-authored-by: Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>

--- inlang.config.js
+++ inlang.config.js
@@ -4,9 +4,21 @@
  * @type {import("@inlang/core/config").DefineConfig}
  */
 export async function defineConfig(env) {
-  const plugin = await env.$import(
-    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
-  );
+  /**
+   * VSCode is using Node 16.14.2, which doesn't support importing from data uris.
+   * Until https://github.com/microsoft/vscode/pull/166430 is merged and stable, this workaround is needed.
+   *
+   * @type {{ readResources: any; writeResources: any; }}
+   */
+  let plugin;
+  // eslint-disable-next-line unicorn/prefer-ternary, no-undef
+  if (typeof process !== "undefined" && process?.version === "v16.14.2") {
+    plugin = await import("./.inlang/inlang-plugin-json.js");
+  } else {
+    plugin = await env.$import(
+      "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+    );
+  }
 
   const pluginConfig = {
     pathPattern: "./resources/{language}.json",
@@ -16,12 +28,25 @@ export async function defineConfig(env) {
     referenceLanguage: "en",
     languages: ["en", "de", "fr"],
     readResources: (args) => {
-      return plugin.readResources({ ...args, ...env, pluginConfig });
       // reading resources
+      return plugin.readResources({ ...args, ...env, pluginConfig });
     },
     writeResources: (args) => {
       // writing resources
       return plugin.writeResources({ ...args, ...env, pluginConfig });
+    },
+    // @ts-ignore
+    ideExtension: {
+      extractMessageOptions: [
+        {
+          id: "As JSX string",
+          callback: (/** @type {string} */ messageId) => `{t("${messageId}")}`,
+        },
+        {
+          id: "As JS string",
+          callback: (/** @type {string} */ messageId) => `t("${messageId}")`,
+        },
+      ],
     },
   };
 }
