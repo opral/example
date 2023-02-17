3fa52f9c3dff3b923cf4ba82492202e35b62e604
Robin Bühler <git+r@obin.ch>
2023-02-17 17:09:14 +0000

    chore: rename finders

--- inlang.config.js
+++ inlang.config.js
@@ -35,7 +35,7 @@ export async function defineConfig(env) {
       return plugin.writeResources({ ...args, ...env, pluginConfig });
     },
     ideExtension: {
-      translationFinders: [
+      messageFinders: [
         async (/** @type {{ "documentText": string; }} */ args) => {
           const regex = /{?t\(['"](?<messageId>\S+)['"]\)}?/gm;
           const str = args.documentText;
