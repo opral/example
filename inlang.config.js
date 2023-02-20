81102b602eff135837465e1a12ee9956e2e73cf3
Robin Bühler <1105080+openscript@users.noreply.github.com>
2023-02-20 02:49:57 +0100

    feat: configure message finders (#12)
    * feat: add inline finder
    
    * chore: rename finders
    
    * chore: clean up temporary types

--- inlang.config.js
+++ inlang.config.js
@@ -28,21 +28,50 @@ export async function defineConfig(env) {
     referenceLanguage: "en",
     languages: ["en", "de", "fr"],
     readResources: (args) => {
-      // reading resources
       return plugin.readResources({ ...args, ...env, pluginConfig });
     },
     writeResources: (args) => {
-      // writing resources
       return plugin.writeResources({ ...args, ...env, pluginConfig });
     },
-    // @ts-ignore
     ideExtension: {
+      messageFinders: [
+        async (/** @type {{ "documentText": string; }} */ args) => {
+          const regex = /{?t\(['"](?<messageId>\S+)['"]\)}?/gm;
+          const str = args.documentText;
+          let match;
+          const result = [];
+
+          while ((match = regex.exec(str)) !== null) {
+            const startLine = (str.slice(0, Math.max(0, match.index)).match(/\n/g) || []).length + 1;
+            const startPos = match.index - str.lastIndexOf('\n', match.index - 1);
+            const endPos = match.index + match[0].length - str.lastIndexOf('\n', match.index + match[0].length - 1);
+            const endLine = (str.slice(0, Math.max(0, match.index + match[0].length)).match(/\n/g) || []).length + 1;
+
+            if (match.groups && 'messageId' in match.groups) {
+              result.push({
+                messageId: match.groups['messageId'],
+                position: {
+                  start: {
+                    line: startLine,
+                    character: startPos
+                  },
+                  end: {
+                    line: endLine,
+                    character: endPos
+                  }
+                }
+              })
+            }
+          }
+          return result;
+        }
+      ],
       extractMessageOptions: [
         {
-          callback: (/** @type {string} */ messageId) => `{t("${messageId}")}`,
+          callback: (messageId) => `{t("${messageId}")}`,
         },
         {
-          callback: (/** @type {string} */ messageId) => `t("${messageId}")`,
+          callback: (messageId) => `t("${messageId}")`,
         },
       ],
     },
