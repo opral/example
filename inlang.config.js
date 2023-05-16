940f6d18dde241f28e05ed64d40b9a544b61737d
Ivan Hofer <ivan.hofer@outlook.com>
2023-05-16 14:00:37 +0200

    install `@inlang/sdk-js`

--- inlang.config.js
+++ inlang.config.js
@@ -3,61 +3,27 @@ export async function defineConfig(env) {
 		'https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js'
 	);
 
+	const { default: sdkPlugin } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin@0.4.5/dist/index.js"
+	);
+
 	const { default: standardLintRules } = await env.$import(
 		'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
 	);
 
 	return {
 		referenceLanguage: 'en',
-		plugins: [pluginJson({ 
-			pathPattern: './resources/{language}.json',
-			variableReferencePattern: ["{", "}"]
-		}), standardLintRules()]
+		plugins: [
+			pluginJson({
+				pathPattern: './resources/{language}.json',
+				variableReferencePattern: ["{", "}"]
+			}),
+			sdkPlugin({
+				languageNegotiation: {
+					strategies: [{ type: "localStorage" }],
+				}
+			}),
+			standardLintRules()
+		]
 	};
 }
-
-// TODO factor out ide extension into own plugin / defined by another plugin
-const ideExtension = {
-	messageReferenceMatchers: [
-		async (/** @type {{ "documentText": string; }} */ args) => {
-			const regex = /(?<!\w){?t\(['"](?<messageId>\S+)['"]\)}?/gm;
-			const str = args.documentText;
-			let match;
-			const result = [];
-
-			while ((match = regex.exec(str)) !== null) {
-				const startLine = (str.slice(0, Math.max(0, match.index)).match(/\n/g) || []).length + 1;
-				const startPos = match.index - str.lastIndexOf('\n', match.index - 1);
-				const endPos =
-					match.index + match[0].length - str.lastIndexOf('\n', match.index + match[0].length - 1);
-				const endLine =
-					(str.slice(0, Math.max(0, match.index + match[0].length)).match(/\n/g) || []).length + 1;
-
-				if (match.groups && 'messageId' in match.groups) {
-					result.push({
-						messageId: match.groups['messageId'],
-						position: {
-							start: {
-								line: startLine,
-								character: startPos
-							},
-							end: {
-								line: endLine,
-								character: endPos
-							}
-						}
-					});
-				}
-			}
-			return result;
-		}
-	],
-	extractMessageOptions: [
-		{
-			callback: (messageId) => `{t("${messageId}")}`
-		},
-		{
-			callback: (messageId) => `t("${messageId}")`
-		}
-	]
-};
