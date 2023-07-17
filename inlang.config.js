a2df41a32cad87dbcbae68eac78a6694338ac522
Ivan Hofer <ivan.hofer@outlok.com>
2023-07-17 17:15:27 +0200

    update all dependencies

--- inlang.config.js
+++ inlang.config.js
@@ -1,16 +1,17 @@
 /**
-* @type { import("@inlang/core/config").DefineConfig }
-*/
+ * @type { import("@inlang/core/config").DefineConfig }
+ */
 export async function defineConfig(env) {
-  const { default: jsonPlugin } = await env.$import('https://cdn.jsdelivr.net/npm/@inlang/plugin-json@3/dist/index.js');
-  
-  const { default: standardLintRules } = await env.$import('https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js');
+	const { default: jsonPlugin } = await env.$import(
+		'https://cdn.jsdelivr.net/npm/@inlang/plugin-json@3/dist/index.js'
+	);
 
-  return {
-    referenceLanguage: 'en',
-    plugins: [
-      jsonPlugin({ pathPattern: 'resources/{language}.json' }),
-      standardLintRules(),
-    ],
-  };
+	const { default: standardLintRules } = await env.$import(
+		'https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js'
+	);
+
+	return {
+		referenceLanguage: 'en',
+		plugins: [jsonPlugin({ pathPattern: 'resources/{language}.json' }), standardLintRules()]
+	};
 }
