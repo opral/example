94ac75eac5a88829f3d9630906d5b3f3e63e1df5
janfjohannes <110794494+janfjohannes@users.noreply.github.com>
2024-02-19 20:31:51 +0200

    inlang: update translations

--- src/routes/todo/model.ts
+++
@@ -1,5 +0,0 @@
-export type Todo = {
-	id: string;
-	title: string;
-	completed: boolean;
-};
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import inlangPlugin from '@inlang/sdk-js/adapter-sveltekit';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [inlangPlugin(), sveltekit()]
-});
